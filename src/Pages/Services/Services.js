fcb86b338aa9c7b01f29e9acdc9da3608def6d4f
TauhidulIslamTuhinKhan <tuhin0411@gmail.com>
2021-10-20 01:01:54 +0600

    revision

--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -1,27 +1,27 @@
 import React, { useEffect, useState } from 'react';
-import SingleService from './SingleService';
+import SingleService from '../SingleService/SingleService';
 
 const Services = () => {
     const [services, setServices] = useState([]);
     useEffect( ()=> {
-        fetch('./services.json')
+        fetch('/services.json')
         .then(res => res.json())
         .then(data => setServices(data));
     }, [])
     return (
         <div className="services">
-                <div className="container">
-                    <h2 className="text-center my-5">Our Services</h2>
-                    <div className="row">
-                        {
-                            services.map(service => <SingleService
-                            key = {service.key}
-                            service = {service}
-                            ></SingleService>)
-                        }
-                    </div>
+            <div className="container">
+                <h2 className="text-center my-5">Our Services</h2>
+                <div className="row">
+                    {
+                        services.map(service => <SingleService
+                        key = {service.key}
+                        service = {service}
+                        ></SingleService>)
+                    }
                 </div>
             </div>
+        </div>
     );
 };
 
--- src/Pages/SingleService/SingleService.js
+++ src/Pages/SingleService/SingleService.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const SingleService = (props) => {
     const {key, serviceName, image, description} = props.service
     return (
-        <div className="col-lg-4 col-md-12">
+        <div className="col-lg-4">
             <div className="card mb-5" style={{ width: '25rem', minHeight: '30rem' }}>
                 <h5 className="card-title text-center">{serviceName}</h5>
                 <img src={image} className="card-img-top" alt="..."/>
