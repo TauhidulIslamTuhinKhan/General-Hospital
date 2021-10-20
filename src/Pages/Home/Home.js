9194e9a2ed4d55db4d8a544f0b1da954eda2354e
TauhidulIslamTuhinKhan <tuhin0411@gmail.com>
2021-10-20 22:21:24 +0600

    responsive done

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -57,7 +57,7 @@ const Home = () => {
             <div className="services">
                 <div className="container">
                     <h2 className="text-center my-5">Our Services</h2>
-                    <div className="row">
+                    <div className="row row-cols-1 row-cols-md-2 g-4">
                         {
                             services.slice(0, 6).map(service => <Service
                             key = {service.key}
